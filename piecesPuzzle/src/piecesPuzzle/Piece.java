f5564954c6478635a1dc7b92123ff212f8e114eb
21804030 <21804030@753c18b0-19e5-4139-a245-8675033f5db4>
2020-10-08 14:47:55 +0000

    Mise en marche de l'application 'jeuAssemblage' réussi

--- piecesPuzzle/src/piecesPuzzle/Piece.java
+++ piecesPuzzle/src/piecesPuzzle/Piece.java
@@ -1,33 +1,59 @@
 package piecesPuzzle;
 
+/**
+ * Classe définissant une pièces générale pouvant être utilisée dans n'importe quel jeu de puzzle.
+ */
 public class Piece {
-
+    /**
+     * Coordonnées de la pièce.
+     */
     private int x, y;
 
+    /**
+     * Constructeur par défaut.
+     * @param x coordonnée en x
+     * @param y coordonnée en y
+     */
     public Piece(int x, int y) {
         this.setX(x);
         this.setY(y);
     }
 
+    @Override
+    public String toString() {
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+    /**
+     * Récupère la coordonnée en x.
+     * @return coordonnée en x
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Met à jour la coordonnée en x.
+     * @param x nouvelle coorodnnée en x
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * Récupère la coordonnée en y.
+     * @return coordonnée en y
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Met à jour la coordonnée en y.
+     * @param y nouvelle coorodnnée en y
+     */
     public void setY(int y) {
         this.y = y;
-    }
-
-    @Override
-    public String toString() {
-        return "(" + this.x + "," + this.y + ")";
     }
 
     public static void main(String[] args){
